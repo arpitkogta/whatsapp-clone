f4acd3364684b50dbc05ded42960cee519e1a9e6
arpitkogta <arpitkogta2001@gmail.com>
2020-12-07 07:55:48 +0530

    Changes made

--- views/src/components/Header.js
+++ views/src/components/Header.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react'
 import ContactCard from './ContactCard';
-import DonutLargeIcon from "@material-ui/icons/DonutLarge";
-import {Avatar,IconButton} from "@material-ui/core";
-import ChatIcon from "@material-ui/icons/Chat";
 
 export default class Header extends Component {
   render() {
@@ -10,12 +7,6 @@ export default class Header extends Component {
     return (
       <div className={this.props.class} onClick={(e)=>{e.preventDefault()}}>
       <ContactCard data={this.props.class === 'left-header' ? {name: currentUser} : {name: reciever || ' '}} disableClick={true}/>
-      <IconButton>
-                <DonutLargeIcon />
-            </IconButton>
-            <IconButton>
-            <ChatIcon />
-            </IconButton>
       </div>
     )
   }
